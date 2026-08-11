644c3a1d84b78582fc42d7cb0a7af1a6732a8c7b
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2026-08-12 02:03:04 +0600

    feat: get banner options

---
+++ main/AdLinkFly/get_banners.php
@@ -0,0 +1,11 @@
+<?php
+$pdo = new PDO("mysql:host=127.0.0.1;dbname=adlinkfly;charset=utf8", "adlinkfly_user", "AdlinkPass2024", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("SELECT name, value FROM options WHERE name LIKE '%banner%'");
+$options = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($options as $opt) {
+    echo $opt['name'] . ": " . $opt['value'] . "\n";
+}
